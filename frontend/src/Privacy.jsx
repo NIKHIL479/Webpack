0cb803dfe5de75c680afcf828e33fb2675a435bc
NIKHIL479 <nikhilkacham15@gmail.com>
2026-01-10 13:56:12 -0500

    added rate limit code

--- frontend/src/Privacy.jsx
+++ frontend/src/Privacy.jsx
@@ -1,9 +1,33 @@
-
+import React, { useState, useEffect } from "react";
 
 export const Privacy = () => {
+ const [text, setText] = useState("");
+  const [debouncedText, setDebouncedText] = useState("");
+
+  //rate limiting technique
+  // Wait before updating debouncedText
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedText(text);
+    }, 2000); // wait 500ms
+
+    return () => {console.log("Clearing timeout:", timer); clearTimeout(timer); };
+  }, [text]);
+
+  // Call API only once
+  useEffect(() => {
+    if (debouncedText) {
+     console.log("API called with:", debouncedText);
+    }
+  }, [debouncedText]);
+
   return (
-    <div>Privacy content!!</div>
-  )
+    <input
+      value={text}
+      onChange={(e) => setText(e.target.value)}
+      placeholder="Search..."
+    />
+  );
 }
 
 export default Privacy;

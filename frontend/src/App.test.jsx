c99d6a82109ec1b1c31ff81eb054002100b08ed2
NIKHIL479 <nikhilkacham15@gmail.com>
2026-01-24 18:03:11 -0500

    rough commit

--- frontend/src/App.test.jsx
+++ frontend/src/App.test.jsx
@@ -1,7 +1,3 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders hello world', () => {
-  render(<App />);
-  expect(screen.getByText(/hello world/i)).toBeInTheDocument();
-});
